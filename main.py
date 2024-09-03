33f3c8973dd47b0114a7863d54f2cc52d0e6117f
IgorMHT <igormatheushorn@gmail.com>
2024-09-02 22:42:00 -0300

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+resultado = 0
+
 @app.get("/")
 async def root():
     print(" Bem vindo a calculadora")
@@ -10,8 +12,16 @@ print("Para começar, digite seu nome")
 
 nome = input("Seu nome: ")
 
-print("Olá", nome, "agora selecione o tipo de conta")
+numero1 = int(input("Olá " + nome + ", digite o primeiro número: "))
+numero2 = int(input("Digite o segundo número: "))
+
+print("Agora selecione o tipo de conta")
 print("Somar")
 print("Subtrair")
-
 tipoConta = input("Desejo ")
+
+if tipoConta == "somar":
+    print("O resultado da conta é ", numero1 + numero2)
+
+if tipoConta == "subtrair":
+    print("O resultado da conta é ", numero1 - numero2)
