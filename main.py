edc000a1fd30c369b176b115ce82e81c7ff5b57f
IgorMHT <igormatheushorn@gmail.com>
2024-09-13 20:59:52 -0300

    Update main.py

--- main.py
+++ main.py
@@ -7,3 +7,18 @@ resultado = 0
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+print("Para começar, digite seu nome")
+
+nome = input("Seu nome: ")
+
+numero1 = int(input("Olá " + nome + ", digite o primeiro número: "))
+numero2 = int(input("Digite o segundo número: "))
+
+print("Agora selecione o tipo de conta")
+print("Somar")
+print("Subtrair")
+tipoConta = input("Desejo ")
+
+if tipoConta == "somar":
+    print("O resultado da conta é ", numero1 + numero2)
